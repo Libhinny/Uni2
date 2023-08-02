83f232cfc0e0a8cd66c706a83dd87294c6f39ad9
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-08-02 16:43:14 -0300

    Create pratica3.c

---
+++ Manipulando arq/pratica3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    char c;
+    char nome_arquivo[20];
+    printf("Informe o nome do arquivo: \n");
+    scanf(" %[^\n]", nome_arquivo); // exemplo: arquivo.txt
+
+    FILE * arquivo = fopen(nome_arquivo, "w"); // abre o arquivo
+    if (arquivo == NULL){
+        printf("Erro ao abrir o arquivo");
+        return 1;
+    }
+    else{
+        printf("Arquivo criado com sucesso! \n");
+    }
+
+    fputs("Hello World!", arquivo); // escreve a mensagem no arquivo
+
+    fclose(arquivo); // fecha o arquivo
+
+    return 0;
+}
