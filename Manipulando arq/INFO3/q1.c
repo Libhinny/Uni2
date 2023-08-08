ff0ce63b5e6641f5887589badced69f2ca27cce8
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-08-08 16:41:06 -0300

    Create q1.c

---
+++ Manipulando arq/INFO3/q1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct funcionarios{
+    int id;
+    char nome[80];
+    float salario;
+}Funcionarios;
+
+int main(void){
+    FILE * s;
+    int n;
+
+    Funcionarios * pessoal = (Funcionarios*)malloc(n*sizeof(Funcionarios));
+        if (pessoal == NULL){
+            printf("Erro na alocação de memória");
+            exit(1);
+        }
+
+    printf("Digite a quantidade de funcionários a serem cadastrados: \n");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        printf("Informe o id: \n");
+        scanf("%d", &pessoal[i].id);
+        printf("Informe o nome: \n");
+        scanf(" %[^\n]", pessoal[i].nome);
+        printf("Informe o salário: \n");
+        scanf("%f", &pessoal[i].salario);
+    }
+
+    s = fopen("info.txt", "wt");
+        if (s == NULL){
+            printf("Erro ao criar o arquivo!\n");
+            fclose(s);
+            return 1;
+        }
+    fprintf(s, "Dados dos funcionários");
+    for(int j = 0; j < n; j++){
+        fprintf("Dados\n Id: %d | Nome: %s | Salário: %.2f \n", pessoal[j].id, pessoal[j].nome, pessoal[j].salario);
+    }
+
+    fclose(s);
+    free(pessoal);
+
+    return 0;
+}
