2feac7a08ab29e71e7a768d534b6fe4152f73b7d
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-08-14 15:17:20 -0300

    Create circulo.h

---
+++ Módulos e compilação/DAT/circulo.h
@@ -0,0 +1,13 @@
+/* TAD: Círculo */
+
+/* Dependência de módulos */
+#include "ponto.h"
+
+/* Tipo exportado */
+typedef struct circulo Circulo;
+
+/* Funções exportadas */
+Circulo* circ_cria (float x, float y, float r);
+void circ_libera (Circulo* c);
+float circ_area (Circulo* c);
+int circ_interior (Circulo* c, Ponto* p);
