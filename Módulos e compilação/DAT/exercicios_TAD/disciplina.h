4f2dc7b6f6a250fcfb6bec068eb3f423558df9b0
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-08-16 16:41:20 -0300

    Create disciplina.h

---
+++ Módulos e compilação/DAT/exercicios_TAD/disciplina.h
@@ -0,0 +1,7 @@
+/* TAD: Disciplina */
+
+typedef struct disciplina Disciplina;
+
+/* Funções importadas */
+Disciplina* cria_disciplina(char nome[], int codigo);
+void exclui_disciplina(Disciplina* disciplina);
