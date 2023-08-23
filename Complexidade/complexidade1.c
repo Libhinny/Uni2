52daf17b64d1843c026e3fbeea83e048382eb8f0
Libhinny Karolayne <101879772+Libhinny@users.noreply.github.com>
2023-08-23 17:24:54 -0300

    Update complexidade1.c

--- Complexidade/complexidade1.c
+++ Complexidade/complexidade1.c
@@ -23,3 +23,48 @@ t(n) = O(n)
 */
 
 // Questão 3
+
+void insertionSort(int arr[], int n){
+    int i, key, j;
+    for (i = 1; i < n; i++){
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+/*
+Pior caso: 
+c1 + c2*(n-1) + c3*(n-1) + c4*(n-1) + [(n^2-n)/2]c5 + [(n^2-n)/2]c6 + [(n^2-n)/2]c7 + c8*(n-1)
+T(n) = a + b*(n-1) + [(n^2-n)/2]*c
+T(n) = O(n^2)
+
+Melhor caso:
+c1 + C2*(n-1) + c3*(n-1) + c4*(n-1) +  + c6*(n-1)
+T(n) = O(n)
+*/
+
+// Questão 4
+
+#include <stdio.h>
+
+unsigned long long factorial (int n ) {
+    if ( n == 0 || n == 1) { // c1
+    return 1; // c2
+    }
+    unsigned long long result = 1; // c3
+    for (int i = 2; i <= n ; i ++) { // c4
+    result *= i ; // c5*
+    }
+    return result; // C6
+}
+int main () {
+int n = 10; // c7
+    unsigned long long result = factorial ( n ); // 
+    printf (" Factorial of %d is %f\n", n , result ); //
+    return 0; // 
+}
